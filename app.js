8de15eeba8ae955cfdc05f3702293986ddba1a3a
John Garcia <johngarciadev@gmail.com>
2021-01-07 18:14:34 -0800

    Updated app.js with code for autocomplete feature.

--- app.js
+++ app.js
@@ -115,17 +115,99 @@ window.addEventListener('click', (e)=> {
     } else {
         hideNotifications();
     }
+
+    closeAutoCompList();
 });
 
 
 
 // for message widget
-const user = document.getElementById('userField');
+const userField = document.getElementById('userField');
 const message = document.getElementById('messageField');
 const send = document.getElementById('send');
+const autoCompContainer = document.querySelector('.autocomplete-container');
+
+users = ['Annie Chen', 'Abe Smith', 'Carla Willet', 'Dale Byrd', 'Dale Brad', 'Dan Oliver', 'John Garcia', 'Juan Garcia', 'Mike Smith', 'Victoria Chambers']
+let currFocus = -1;
+
+function getSearches(arr, queryStr) {
+    const searches = []
+    for (let i = 0; i < arr.length; i++) {
+    if ( arr[i].toLowerCase().startsWith(queryStr.toLowerCase()) ) {
+            searches.push(arr[i]);
+        }
+    }
+
+    return searches;
+}
+
+function closeAutoCompList() {
+    const currList = document.querySelector('.autocomplete-list');
+    if (currList) {
+        autoCompContainer.removeChild(currList);
+    }
+}
+
+function addActive(collection) {
+    removeActive(collection);
+    if (currFocus >= collection.length) currFocus = 0
+    if (currFocus < 0 ) currFocus = collection.length - 1
+    collection[currFocus].classList.add('autocomplete-active')
+}
+
+function removeActive(collection) {
+    for (let i = 0; i < collection.length; i++) {
+        collection[i].classList.remove('autocomplete-active');
+    }
+}
+
+
+userField.addEventListener('input', ()=> {
+    const query = userField.value.trim();
+    if (query) {
+        const searches = getSearches(users, query)
+
+        if (searches.length > 0) {
+            closeAutoCompList();
+            // build new list holding searches
+            const autoCompList = document.createElement('UL');
+            autoCompList.classList.add('autocomplete-list');
+    
+            for (let i = 0; i < searches.length; i++) {
+                const li = document.createElement('LI');
+                li.classList.add('autocomplete-item');
+                li.textContent = searches[i];
+                li.addEventListener('click', (e)=> {
+                    userField.value = e.target.textContent;
+                    closeAutoCompList();
+                });
+                autoCompList.appendChild(li);
+            }
+            autoCompContainer.appendChild(autoCompList);
+        }
+    }  
+});
+
+userField.addEventListener('keydown', (e)=> {
+    const currList = document.getElementsByClassName('autocomplete-item');
+    if (currList.length > 0) {
+        if (e.key === 'ArrowUp') {
+            currFocus--;
+            addActive(currList);
+        } else if (e.key === 'ArrowDown') {
+            currFocus++;
+            addActive(currList);
+        } else if (e.key === 'Enter') {
+            e.preventDefault();
+            if (currFocus > -1) {
+                currList[currFocus].click();
+            }
+        }
+    }
+});
 
 send.addEventListener('click', ()=> {
-    const userValue = user.value.trim();
+    const userValue = userField.value.trim();
     const messageValue = message.value.trim();
 
     if (userValue === '' && messageValue === '') {
